0b35506ff9fab04d30951c0420a67c12f9ed8b78
Emiliopg91 <ojosdeserbio@gmail.com>
2025-01-22 21:08:03 +0100

    Change game info model

--- src/utils/listeners.ts
+++ src/utils/listeners.ts
@@ -12,7 +12,7 @@ import { BackendUtils } from './backend';
 export class Listeners {
   private static unsubscribeGameEvents: (() => void) | undefined = undefined;
   private static unsubscribeIncommingRequest: (() => void) | undefined = undefined;
-  public static runningApplications: Array<string> = [];
+  public static runningApplications: Array<{ id: number; name: string }> = [];
 
   public static bind(): void {
     Listeners.unsubscribeGameEvents = EventBus.subscribe(EventType.GAME_LIFE, (e: EventData) => {
@@ -20,14 +20,13 @@ export class Listeners {
       Logger.info('New game event:', event);
       event.getDetails().then((game) => {
         if (event.isRunning()) {
-          Listeners.runningApplications.push(game.getDisplayName());
-          BackendUtils.emitEvent('launch_game', game.getDisplayName());
+          Listeners.runningApplications.push({ id: game.getGameId(), name: game.getDisplayName() });
+          BackendUtils.emitEvent('launch_game', game.getGameId(), game.getDisplayName());
         } else {
-          const index = Listeners.runningApplications.indexOf(game.getDisplayName());
-          if (index !== -1) {
-            Listeners.runningApplications.splice(index, 1);
-            BackendUtils.emitEvent('stop_game', game.getDisplayName());
-          }
+          Listeners.runningApplications = Listeners.runningApplications.filter(
+            (e) => e.id != game.getGameId()
+          );
+          BackendUtils.emitEvent('stop_game', game.getGameId(), game.getDisplayName());
         }
       });
     }).unsubscribe;
@@ -36,6 +35,14 @@ export class Listeners {
       'get_running_games',
       (id: string) => {
         BackendUtils.sendResponse(id, 'get_running_games', Listeners.runningApplications);
+      }
+    );
+
+    Listeners.unsubscribeIncommingRequest = Backend.backend_handle(
+      'get_app_details',
+      (id: string) => {
+        //appDetailsStore.GetAppDetails(990080).strLaunchOptions
+        //SteamClient.Apps.SetAppLaunchOptions(990080, "EVK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.i686.json:/usr/share/vulkan/icd.d/nvidia_icd.x86_64.json %command%")
       }
     );
   }
